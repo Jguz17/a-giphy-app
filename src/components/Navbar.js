ef51e5cc5bd9efebedf4ca0d98c36947062fa7a7
jguz17 <johnathanguzman17@gmail.com>
2020-10-01 11:20:26 -0500

    setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <h1><img src={require('../images/giphy-logo.png')}/> A GIPHY APP</h1>
+        </div>
+    )
+}
+
+export default Navbar
